dcb45d42cd3d71d14c02461c67d8425cc9b3c9aa
Khalil Najjar <knajjars@gmail.com>
2020-01-22 22:37:51 +0100

    first structure

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript'
+  ],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-unreachable': 'warn',
+    'no-implicit-coercion': 'warn',
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always-and-inside-groups'
+      }
+    ]
+  },
+  parserOptions: {
+    project: `./tsconfig.json`
+  }
+};
