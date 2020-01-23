13a0de63410258d27f0833b5189c2a14bfe458cc
Khalil Najjar <knajjars@gmail.com>
2020-01-23 22:59:32 +0100

    webhook test

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import { port } from './configs/variables';
 
 const app = express();
 
-app.get('/', (_req, res) => res.send('Hello World!'));
+app.get('/', (_req, res) => res.send('Hello World! I am Aprta.'));
 
 app.listen(port, () => console.log(`Aprta is live on port ${port}!`));
