8fc0286d8bc294b78661b1ec4de4f5d698560ff0
Mauricio Mourraille <mourraille@me.com>
2020-01-27 21:04:20 +0100

    Apartment model.

---
+++ src/common/dictionary/ApartmentType.ts
@@ -0,0 +1,5 @@
+export enum ApartmentType {
+  EntirePlace = 'entirePlace',
+  PrivateRoom = 'privateRoom',
+  SharedRoom = 'sharedRoom'
+}
---
+++ src/models/Apartment.ts
@@ -0,0 +1,37 @@
+import mongoose, { mongo } from 'mongoose';
+
+import { User as UserModel, UserRole } from '../common';
+
+import { ModelReference } from './modelReference';
+
+const UserSchema = new mongoose.Schema(
+  {
+    ownerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: ModelReference.User
+    },
+    title: { type: String, required: true },
+    lastName: String,
+    email: { type: String, required: true, unique: true, index: true },
+    role: { type: String, enum: Object.values(UserRole), default: UserRole.Tenant },
+    phone: { type: String, unique: true },
+    password: { type: String, required: true },
+    requests: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: ModelReference.Request
+      }
+    ]
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  }
+);
+
+UserSchema.index({ email: 1 });
+
+const User: mongoose.Model<UserModel> = mongoose.model(ModelReference.User, UserSchema);
+export default User;
